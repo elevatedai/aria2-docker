7f9bb5e99075b459c9b5f75102d2c5717a241d6a
divyam234 <47589864+divyam234@users.noreply.github.com>
2024-09-11 15:11:40 +0530

    Update start.sh script to remove unnecessary echo statement and fix path substitution

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,6 @@ $ARIA2_CONF_DIR/update_tracker.sh "$ARIA2_CONF_DIR/aria2.conf"
 
 echo "Trackers Updated"
 
-echo "[INFO] Start aria2 with rpc-secret"
-
 sed -i 's|#ARIA2_CONF_DIR|'"$ARIA2_CONF_DIR"'|g' "$ARIA2_CONF_DIR/aria2.conf"
 
 aria2c --conf-path="$ARIA2_CONF_DIR/aria2.conf" --rpc-secret="${RPC_SECRET}" --dir="${ARIA2_DOWNLOAD_DIR}"
